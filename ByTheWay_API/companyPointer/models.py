373fa75848374d32be297d2493fa022f48496089
yakub miastoff <iakub.urban@gmail.com>
2018-04-12 20:17:56 +0200

    create serializers.py file, add companyPointer app to settings, create first models

--- ByTheWay_API/companyPointer/models.py
+++ ByTheWay_API/companyPointer/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=100, blank=False, unique=True)
+    description = models.TextField()
+    logo = models.ForeignKey(LogoUpload, related_name="company_logo", on_delete=models.CASCADE)
+    pos_lat = models.FloatField()
+    pos_lon = models.FloatField()
+    tags = models.ForeignKey(Tag, related_name="company_tag", on_delete=models.CASCADE)
+    type = models.ForeignKey(CompanyType, related_name="company_type", on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class CompanyType(models.Model):
+    name = models.CharField(max_length=50, blank=False, unique=True)
+
+
+class LogoUpload(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, to_field='id')
+    datafile = models.FileField()
