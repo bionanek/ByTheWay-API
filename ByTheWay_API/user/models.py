281101f69a906f06555472496dbfe09bb28270f2
yakub miastoff <iakub.urban@gmail.com>
2018-04-15 16:24:20 +0200

    implement profile model and create migration of it

--- ByTheWay_API/user/models.py
+++ ByTheWay_API/user/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from companyPointer.models import Company, Tag
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, to_field="id", on_delete=models.CASCADE)
+    birthdate = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    related_company = models.ForeignKey(Company, blank=True, default=None, null=True, on_delete=models.CASCADE)
+    interests = models.ManyToManyField(Tag, related_name="profile_interests")
+    favourites = models.ManyToManyField(Company, related_name="profile_favourites") # TODO: implement algorithm and helper app that will determine which companies are favourites
+    last_latitude = models.FloatField()
+    last_longitude = models.FloatField()
